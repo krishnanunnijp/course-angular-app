7fdf958aaaf67bb60ad678b59bc17eaa44ae95aa
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-25 17:37:35 +0530

    course add  page added

---
+++ src/app/course-navbar/course-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-navbar',
+  templateUrl: './course-navbar.component.html',
+  styleUrls: ['./course-navbar.component.css']
+})
+export class CourseNavbarComponent {
+
+}
