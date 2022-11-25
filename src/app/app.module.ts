e8c9703cff41ab04e1223b3a955a5093d51b1830
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-25 17:14:59 +0530

    course view page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseViewComponent } from './course-view/course-view.component';
+
+const myRoute: Routes=[
+  {
+    path: "",
+    component: CourseViewComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
